e84f8e680e798966c0fc4a37bed4d3b9eb839ecc
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2022-11-27 19:43:26 -0400

    Update background.js

--- background.js
+++ background.js
@@ -11,8 +11,19 @@ url_list = [
 	"*://*.zedo.com/*",
     "*://*.moat.com/*",
     "*://*.amazon-adsystem.com/*",
-    "*://*.ads.linkedin.com/*"
+    "*://*.ads.linkedin.com/*",
+    "*://*.insight.adsrvr.org/*",
+    "*://*.outbrain.com/*"
 ]
+
+async function fetchData() {
+    const res = await fetch ("https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt");
+    const record = await res.text();
+    document.getElementById("date").innerHTML=record.data[0].date;
+    document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { 
