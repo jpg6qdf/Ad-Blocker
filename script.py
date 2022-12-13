93c264fa8cd7f83bcd290929328a3da890bf17f0
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2022-12-13 02:26:12 -0400

    updated output_file format

--- script.py
+++ script.py
@@ -1,15 +1,18 @@
-import urllib.request, re
+import urllib.request
+import re
 
 req = urllib.request.Request(
-    url = "https://easylist.to/easylist/easylist.txt", 
+    url="https://easylist.to/easylist/easylist.txt",
     headers={'User-Agent': 'Mozilla/5.0'}
 )
 file = urllib.request.urlopen(req)
 output_file = open('output_file.txt', 'w')
+output_file.write("url_list = [" + "\n")
 for line in file:
     decoded_line = str(line.decode("utf-8"))
     if '||' == decoded_line[0:2] and '^' in decoded_line and '*' not in decoded_line:
         last_index = decoded_line.index('^')
         url = '*://*.' + decoded_line[2:last_index].strip() + '/*'
-        if re.search('[a-zA-Z]', url) != None:
-            output_file.write("'" + url + "'," + "\n")
+        if re.search('[a-zA-Z]', url) is not None:
+            output_file.write("\t" + "'" + url + "'," + "\n")
+output_file.write("]")
