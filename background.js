8007800070265cec16647f5ac6f6d33554b97b8a
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2022-12-14 23:20:11 -0400

    ad block with on off

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
-var url_list = []
+var extension_status = "on";
 
-async function fetchData() {
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({state: 'on'})
+  });
+
+async function extensionOn() {
     const res = await fetch ("https://easylist.to/easylist/easylist.txt");
     const record = await res.text();
     var lines = record.split('\n');
+    var url_list = []
     for (var i = 1; i < lines.length; i++){
         line = lines[i];
 
@@ -14,13 +19,32 @@ async function fetchData() {
         }
     }
     chrome.webRequest.onBeforeRequest.addListener(
-        function(details) { 
-            return {cancel: true}; 
+        function(details) {
+            console.log("on")
+            if (extension_status == "on"){
+                return {cancel: true}; 
+            }
+            else{
+                return {cancel: false}
+            }
         },
         { urls: url_list },
         ["blocking"]
     );
 }
 
-fetchData();
+function extensionOff(){}
 
+chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.storage.local.get('state', function(data) {
+      if(data.state == 'on') {
+        chrome.storage.local.set({state: 'off'});
+        extension_status = "on"
+        chrome.tabs.reload(extensionOn)
+      } else {
+        chrome.storage.local.set({state: 'on'})
+        extension_status = "off"
+        chrome.tabs.reload(extensionOff)
+      }
+    });
+  });
